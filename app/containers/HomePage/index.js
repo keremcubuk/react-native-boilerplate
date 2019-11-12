1e710fe6b8d1a0ee330b04b3be0ae4a9992973c0
Kerem Çubuk <kerem.cubuk@eteration.com>
2019-11-12 23:19:33 +0300

    Minor: Updates

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,8 +7,6 @@
 
 import React from 'react';
 import { Text } from 'react-native';
-// import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 export default function HomePage() {
   return <Text>Welcome to IveBeen</Text>;
