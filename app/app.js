55a032b6a36c540c3bfddab2afb76ade4af01002
Kerem Çubuk <keremcubuk@hotmail.com>
2019-11-17 17:47:48 +0300

    Remove: NativeBase from project

--- app/app.js
+++ app/app.js
@@ -8,15 +8,12 @@
 // Import all the third party stuff
 import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
-import { Root, StyleProvider } from 'native-base';
 
 // Import root app
 import App from './containers/App';
 
 import configureStore from './configureStore';
 import createReducer from './reducers';
-import getTheme from 'theme/components';
-import commonColor from 'theme/variables/commonColor';
 
 // Create redux store with history
 const initialState = {};
@@ -32,17 +29,13 @@ function AppRoot() {
   }, []);
 
   return (
-    <StyleProvider style={getTheme(commonColor)}>
-      <Root>
-        <Provider store={store}>
-          <App
-            ref={nav => {
-              this.navigator = nav;
-            }}
-          />
-        </Provider>
-      </Root>
-    </StyleProvider>
+    <Provider store={store}>
+      <App
+        ref={nav => {
+          this.navigator = nav;
+        }}
+      />
+    </Provider>
   );
 }
 
