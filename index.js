7991fa2b6c0ca3cc91f9add73f577603289b7210
Kerem Çubuk <keremcubuk@hotmail.com>
2019-11-18 22:40:02 +0300

    Fix: RN release mode crash

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import 'react-native-gesture-handler'; // Navigation Release Mode Crash issue #320
 import { AppRegistry } from 'react-native';
 import App from './app/app';
 import { name as appName } from './app.json';
