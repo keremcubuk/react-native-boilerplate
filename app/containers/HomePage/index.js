1b630eb8cb92774a83fb5d94449a81afadd995ef
Kerem Çubuk <kerem.cubuk@eteration.com>
2020-04-04 13:35:40 +0300

    Add: Homepage React Native Intro

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,13 +1,55 @@
 /*
  * HomePage
  *
- * This is the first thing users see of our App, at the '/' route
+ * This is the second thing users see of our App, at the '/' route
  *
  */
 
 import React from 'react';
-import { Text } from 'react-native';
+import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
+
+import { Header, LearnMoreLinks, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import { styles } from './styles';
 
 export default function HomePage() {
-  return <Text>Welcome to templateReactNative</Text>;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
 }
